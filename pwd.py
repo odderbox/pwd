34a14474df9d0287b7ca74aefe392a65f6aee220
Derek Chen <odderbox@gmail.com>
2019-12-01 23:55:23 +0800

    change alert

--- pwd.py
+++ pwd.py
@@ -1,13 +1,14 @@
 password = 'a123456'
 chance = 3
-while True:
+while chance > 0:
+	chance -= 1
 	text = input('請輸入密碼：')
 	if text == password:
-		print('登入成功')
+		print('登入成功！')
 		break
 	else:
-		chance -= 1
-		if chance == 0:
-			break
-		else:	
-			print('密碼錯誤！還有', chance, '次機會')
+		print('密碼錯誤！')
+		if chance > 0:
+			print('還有', chance, '次機會！')
+		else:
+			print('帳號已鎖定！')
